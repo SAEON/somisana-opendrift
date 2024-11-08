f1b300a1fb9e3cbbc3999f96fff97065247b9f39
GilesFearon <77445622+GilesFearon@users.noreply.github.com>
2024-11-08 14:05:53 +0200

    Update config_oil.py - east coast blowout

--- configs/config_oil.py
+++ configs/config_oil.py
@@ -12,7 +12,7 @@
 # (these are not used in local runs)
 #
 # give a name for your configuration
-config_name='test_oil'
+config_name='test_oil_blowout'
 #
 # define the date when the croco runs were initialised, in format YYYYMMDD_HH 
 run_date='20241106_12'
@@ -22,30 +22,30 @@ run_date='20241106_12'
 # -----------
 #
 # coordinates of the release (in geographical degrees)
-lon_release=25.772602
-lat_release=-33.869797
+lon_release=31.779959
+lat_release=-30.539622
 #
 # depth of the release
 # for a subsurface release you can also specify a distance off the seabed like z='seafloor+100' for 100m off the bottom
-z=0
+z='seafloor+100'
 #
 # radius to be used in initialising the particles
 # particles will be initialised around 'lon_release,lat_release' using a standard deviation of 'radius'
 # this allows for some initial spreading at location of the initialised particles 
 # for a subsea blowout this could be hundereds of meters, but a surface spill it will be small, in the order of meters
-radius=100
+radius=1000
 #
 # specify the oil type - important for weathering properties
 # Can choose any oil name from https://adios.orr.noaa.gov/oils/
 # Or some Norgegain oils from https://opendrift.github.io/oil_types.html
 # Or a few other generic oil types added as part of opendrift, such as 'GENERIC INTERMEDIATE FUEL OIL 180'
-oil_type='GENERIC INTERMEDIATE FUEL OIL 180'
+oil_type='Norman Wells'
 #
 # start time of spill, in format YYYYMMDD_HH, in UTC
 release_start_time='20241102_00'
 #
 # duration of the release of oil in hours (this can't be zero!)
-release_dur=3
+release_dur=48
 #
 # volume of oil spilled in m3
 # This is not used directly in the model - it's only used here to get the oil flow rate below
@@ -53,12 +53,13 @@ release_dur=3
 oil_volume=50
 #
 # oil flow rate in m3/hr
-oil_flow_rate=oil_volume/release_dur
+# oil_flow_rate=oil_volume/release_dur
+oil_flow_rate=40
 #
 # initial droplet sizes
 # this get's over-ridden by the wave entrainment DSD when oil is submerged into the water column due to wave entrainment
 # so this input is not important for a surface release
-dsd='uniform' # 'uniform' or 'lognormal'
+dsd='lognormal' # 'uniform' or 'lognormal'
 # if 'uniform' dsd_param1 is the min diamter (m)
 # if 'lognormal' dsd_param1 is the mean diameter (m)
 dsd_param1=0.001
