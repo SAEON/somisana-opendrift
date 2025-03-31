0121c82c5b0ce7418e71deae447ee5e81d933cdc
GilesFearon <77445622+GilesFearon@users.noreply.github.com>
2025-03-31 14:54:47 +0200

    Update config_oceandrift.py

--- configs/config_oceandrift.py
+++ configs/config_oceandrift.py
@@ -12,7 +12,7 @@
 # (these are not used in local runs)
 #
 # give a name for your configuration
-config_name='Agulhas_spill_Gas_Capricorn'
+config_name='Agulhas_spill_Gas_Capricorn_2'
 #
 # define the date when the croco runs were initialised, in format YYYYMMDD_HH 
 run_date='20250329_00'
@@ -32,7 +32,7 @@ z=0
 # radius to be used in initialising the particles
 # particles will be initialised around 'lon_release,lat_release' using a standard deviation of 'radius'
 # this allows for some initial spreading at location of the initialised particles 
-radius=100
+radius=10000
 #
 # start time of release, in format YYYYMMDD_HH, in UTC
 release_start_time='20250327_15'
