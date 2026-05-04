a318596e8348aa196771a5ec9687aee83743d15c
GRautenbach <g.rautenbach@saeon.nrf.ac.za>
2026-05-04 14:48:25 +0200

    Testing operational hab-advection tool.

--- configs/config_phytodrift.py
+++ configs/config_phytodrift.py
@@ -12,7 +12,7 @@
 # (these are not used in local runs)
 #
 # give a name for your configuration
-config_name='habdrift'
+config_name='phytodrift'
 #
 # define the date when the croco runs were initialised, in format YYYYMMDD_HH 
 run_date='20250522_00'
